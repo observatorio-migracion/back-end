84e2ae2499c1b78bfba380c9684b825c2afeb851
KABU09 <eduardoayalesleon@gmail.com>
2021-03-19 13:33:14 -0600

    first

---
+++ config/plugins.js
@@ -0,0 +1,5 @@
+module.exports = ({ env }) => ({
+    sentry: {
+      dsn: "https://edda0fa5764e4a118980f70041289102@o528378.ingest.sentry.io/5645697",//env('SENTRY_DSN'),
+    },
+  });
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e4b4bb92-2345-4f2d-902e-2bcc0af88b00'
+};
