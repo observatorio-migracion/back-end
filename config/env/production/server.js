25069ce340f8e8ead9d5d1bf25882adc7d6855c6
Jim Zúñiga <jimd.zuniga@gmail.com>
2021-11-11 18:11:50 -0600

    Configuración para Nginx en ambiente de producción

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,8 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', '127.0.0.1'),
   port: env.int('PORT', 1337),
+  url: 'http://127.0.0.1/api',
+  proxy: true,
   admin: {
-    auth: {
+    url: 'http://127.0.0.1/panel-de-administrador',auth: {
       secret: env('ADMIN_JWT_SECRET', '0d2ef5a69cb65f637f5b9c58f945034c'),
     },
   },
