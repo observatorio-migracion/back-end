44458fa3f97138e5fa85f4dc20621ca47f10c3ac
Jim Zúñiga <jimd.zuniga@gmail.com>
2021-11-16 17:19:12 -0600

    Configuración de servidor Node para conectarse con Nginx.

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,11 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '127.0.0.1'),
   port: env.int('PORT', 1337),
-  url: 'http://127.0.0.1/api',
-  proxy: true,
+  url: 'http://localhost/api',
   admin: {
-    url: 'http://127.0.0.1/panel-de-administrador',auth: {
-      secret: env('ADMIN_JWT_SECRET', '0d2ef5a69cb65f637f5b9c58f945034c'),
-    },
+    url: 'http://localhost/panel-de-administrador',
   },
 });
